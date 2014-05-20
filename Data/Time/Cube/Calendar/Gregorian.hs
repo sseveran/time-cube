e8090c6026c1cb192803faa7cfb50bfe1a6052fa
Enzo Haussecker <ehaussecker@gmail.com>
2014-05-20 16:33:06 -0700

    revised stability and portability

--- Data/Time/Cube/Calendar/Gregorian.hs
+++ Data/Time/Cube/Calendar/Gregorian.hs
@@ -11,8 +11,8 @@
 -- Copyright   : Copyright (c) 2014, Alpha Heavy Industries, Inc. All rights reserved.
 -- License     : Apache License, Version 2.0
 -- Maintainer  : Enzo Haussecker <enzo@ucsd.edu>
--- Stability   : Experimental
--- Portability : Untested
+-- Stability   : Stable
+-- Portability : Portable
 --
 -- The Gregorian calendar system.
 --
