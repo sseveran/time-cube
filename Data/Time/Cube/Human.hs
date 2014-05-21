e4141036a65f5c0c5b473528f72b71b631148be6
Enzo Haussecker <ehaussecker@gmail.com>
2014-05-20 19:31:22 -0700

    using nathan component class methodology

--- Data/Time/Cube/Human.hs
+++ Data/Time/Cube/Human.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE KindSignatures #-}
-{-# OPTIONS -Wall           #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS -Wall         #-}
 
 -- |
 -- Module      : Data.Time.Cube.Human
@@ -10,68 +9,24 @@
 -- Stability   : Stable
 -- Portability : Portable
 --
--- Classes for human-readable dates and time.
-module Data.Time.Cube.Human where
+-- A class for human-readable timestamps.
+module Data.Time.Cube.Human (
 
-import Data.Time.Cube.Base (Calendar)
-import Data.Time.Cube.Struct
+ -- ** Class
+       Human(..)
 
-class HumanDate x where
+     ) where
 
-   -- |
-   -- Compose a timestamp from date components.
-   fromDateStruct :: DateStruct (cal :: Calendar) -> x
+class Human x where
 
    -- |
-   -- Decompose a timestamp into date components.
-   toDateStruct :: x -> DateStruct (cal :: Calendar)
-
-class HumanTime x where
+   -- Define the human-readable components of a timestamp.
+   data Components x :: *
 
    -- |
-   -- Compose a timestamp from time components.
-   fromTimeStruct :: TimeStruct -> x
+   -- Compose a timestamp from human-readable components.
+   pack :: Components x -> x
 
    -- |
-   -- Decompose a timestamp into time components.
-   toTimeStruct :: x -> TimeStruct
-
-class (HumanDate x, HumanTime x) => HumanDateTime x where
-
-   -- |
-   -- Compose a timestamp from date and time components.
-   fromDateTimeStruct :: DateTimeStruct (cal :: Calendar) -> x
-
-   -- |
-   -- Decompose a timestamp into date and time components.
-   toDateTimeStruct :: x -> DateTimeStruct (cal :: Calendar)
-
-class HumanLocalDate x where
-
-   -- |
-   -- Compose a timestamp from local date components.
-   fromLocalDateStruct :: LocalDateStruct (cal :: Calendar) -> x
-
-   -- |
-   -- Decompose a timestamp into local date components.
-   toLocalDateStruct :: x -> LocalDateStruct (cal :: Calendar)
-
-class HumanLocalTime x where
-
-   -- |
-   -- Compose a timestamp from local time components.
-   fromLocalTimeStruct :: LocalTimeStruct -> x
-
-   -- |
-   -- Decompose a timestamp into local time components.
-   toLocalTimeStruct :: x -> LocalTimeStruct
-
-class (HumanLocalDate x, HumanLocalTime x) => HumanLocalDateTime x where
-
-   -- |
-   -- Compose a timestamp from local date and time components.
-   fromLocalDateTimeStruct :: LocalDateTimeStruct (cal :: Calendar) -> x
-
-   -- |
-   -- Decompose a timestamp into local date and time components.
-   toLocalDateTimeStruct :: x -> LocalDateTimeStruct (cal :: Calendar)
+   -- Decompose a timestamp into human-readable components.
+   unpack :: x -> Components x
