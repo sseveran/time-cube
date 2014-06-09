d80866347331eda8f66e9668261192d31bf7e8ba
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-09 08:58:07 -0700

    Added Unix datestamp type

---
+++ Data/Time/Cube/Unix/Date.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE KindSignatures #-}
+{-# OPTIONS -Wall           #-}
+
+-- |
+-- Module      : Data.Time.Cube.Unix.Date
+-- Copyright   : Copyright (c) 2014, Alpha Heavy Industries, Inc. All rights reserved.
+-- License     : Apache License, Version 2.0
+-- Maintainer  : Enzo Haussecker <enzo@ucsd.edu>
+-- Stability   : Stable
+-- Portability : Portable
+--
+-- The Unix datestamp.
+module Data.Time.Cube.Unix.Date (
+
+ -- ** Type
+       UnixDate(..)
+
+     ) where
+
+import Data.Int (Int32)
+import Data.Time.Cube.Base (Calendar)
+import GHC.Generics (Generic)
+
+-- |
+-- Days since Unix epoch. 
+newtype UnixDate (cal :: Calendar) = UnixDate {getBase :: Int32}
+    deriving (Eq, Generic, Ord)
