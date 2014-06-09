5c541a631073d07aff292348fdbcd6c89912a293
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-09 15:57:03 -0700

    Added get current date function

--- Data/Time/Cube/Unix/Date.hs
+++ Data/Time/Cube/Unix/Date.hs
@@ -12,21 +12,32 @@
 -- Stability   : Stable
 -- Portability : Portable
 --
--- The Unix datestamp.
+-- The Unix date.
 module Data.Time.Cube.Unix.Date (
 
  -- ** Type
        UnixDate(..)
+
+ -- ** Current
+     , getCurrentUnixDate
 
      ) where
 
 import Control.DeepSeq (NFData)
 import Data.Int (Int32)
 import Data.Time.Cube.Base (Calendar)
+import Data.Time.Cube.Unix (C'timeval(..), getTimeOfDay)
 import Foreign.Storable (Storable)
 import GHC.Generics (Generic)
 
 -- |
 -- Days since Unix epoch.
 newtype UnixDate (cal :: Calendar) = UnixDate {getBase :: Int32}
-    deriving (Eq, Generic, NFData, Ord, Storable)
+   deriving (Eq, Generic, NFData, Ord, Storable)
+
+-- |
+-- Get the current Unix date from the system clock.
+getCurrentUnixDate :: IO (UnixDate (cal :: Calendar))
+getCurrentUnixDate =
+   getTimeOfDay >>= \ (C'timeval base _) ->
+   return $! UnixDate . fromIntegral $ base `div` 86400
