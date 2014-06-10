f9f42d2c26a4e64a7d06f2675343b11a53c197d9
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-10 01:26:30 -0700

    Added time zone parameterization

--- Data/Time/Cube/Unix.hs
+++ Data/Time/Cube/Unix.hs
@@ -23,15 +23,16 @@ module Data.Time.Cube.Unix (
 import Control.DeepSeq (NFData)
 import Data.Int (Int32, Int64)
 import Data.Time.Cube.Base (Calendar)
+import Data.Time.Cube.Zone (TimeZone)
 import Foreign.Storable (Storable)
 import GHC.Generics (Generic)
 
 -- |
 -- Days since Unix epoch.
-newtype UnixDate (cal :: Calendar) = UnixDate {getDate :: Int32}
+newtype UnixDate (cal :: Calendar) (tz :: TimeZone) = UnixDate {getDate :: Int32}
    deriving (Eq, Generic, NFData, Ord, Storable)
 
 -- |
 -- Seconds since Unix epoch.
-newtype UnixTime (cal :: Calendar) = UnixTime {getTime :: Int64}
+newtype UnixTime (cal :: Calendar) (tz :: TimeZone) = UnixTime {getTime :: Int64}
    deriving (Eq, Generic, NFData, Ord, Storable)
