9b3b98823ccac780f75d75089c1134b23b87ba1a
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-09 18:22:30 -0700

    Merged struct and base modules

--- Data/Time/Cube/Base.hs
+++ Data/Time/Cube/Base.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
 {-# OPTIONS -Wall                       #-}
 
 -- |
@@ -38,6 +41,14 @@ module Data.Time.Cube.Base (
      , Nanos(..)
      , Picos(..)
 
+ -- ** Structs
+     , DateStruct(..)
+     , TimeStruct(..)
+     , DateTimeStruct(..)
+     , LocalDateStruct(..)
+     , LocalTimeStruct(..)
+     , LocalDateTimeStruct(..)
+
  -- ** Fractions
      , properFracMillis
      , properFracMicros
@@ -48,6 +59,7 @@ module Data.Time.Cube.Base (
 
 import Control.DeepSeq (NFData)
 import Data.Int (Int32, Int64)
+import Data.Time.Cube.Zone (TimeZone)
 import GHC.Generics (Generic)
 import Text.Printf (PrintfArg)
 
@@ -88,7 +100,7 @@ data Calendar =
 
 -- |
 -- System for numbering years.
-data family Era (cal :: Calendar) :: *
+data family Era (calendar :: Calendar) :: *
 
 -- |
 -- System origin.
@@ -103,7 +115,7 @@ newtype Year = Year {getYear :: Int32}
 
 -- |
 -- Month.
-data family Month (cal :: Calendar) :: *
+data family Month (calendar :: Calendar) :: *
 
 -- |
 -- Day.
@@ -112,7 +124,7 @@ newtype Day = Day {getDay :: Int32}
 
 -- |
 -- Day of week.
-data family DayOfWeek (cal :: Calendar) :: *
+data family DayOfWeek (calendar :: Calendar) :: *
 
 -- |
 -- Hour.
@@ -148,6 +160,121 @@ newtype Nanos = Nanos {getNanos :: Int64}
 -- Picosecond.
 newtype Picos = Picos {getPicos :: Int64}
    deriving (Bounded, Enum, Eq, Generic, Integral, NFData, Num, Ord, PrintfArg, Real, Show)
+
+-- |
+-- A struct with date components.
+data DateStruct (calendar :: Calendar) =
+     DateStruct
+       { _d_year :: {-# UNPACK #-} !Year
+       , _d_mon  ::                !(Month calendar)
+       , _d_mday :: {-# UNPACK #-} !Day
+       , _d_wday ::                !(DayOfWeek calendar)
+       } deriving Generic
+
+-- |
+-- A struct with time components.
+data TimeStruct =
+     TimeStruct
+       { _t_hour :: {-# UNPACK #-} !Hour
+       , _t_min  :: {-# UNPACK #-} !Minute
+       , _t_sec  :: {-# UNPACK #-} !Double
+       } deriving (Eq, Generic, Ord, Show)
+
+-- |
+-- A struct with date and time components.
+data DateTimeStruct (calendar :: Calendar) =
+     DateTimeStruct
+       { _dt_year :: {-# UNPACK #-} !Year
+       , _dt_mon  ::                !(Month calendar)
+       , _dt_mday :: {-# UNPACK #-} !Day
+       , _dt_wday ::                !(DayOfWeek calendar)
+       , _dt_hour :: {-# UNPACK #-} !Hour
+       , _dt_min  :: {-# UNPACK #-} !Minute
+       , _dt_sec  :: {-# UNPACK #-} !Double
+       } deriving Generic
+
+-- |
+-- A struct with date and location components.
+data LocalDateStruct (calendar :: Calendar) =
+     LocalDateStruct
+       { _ld_year :: {-# UNPACK #-} !Year
+       , _ld_mon  ::                !(Month calendar)
+       , _ld_mday :: {-# UNPACK #-} !Day
+       , _ld_wday ::                !(DayOfWeek calendar)
+       , _ld_zone ::                !TimeZone
+       } deriving Generic
+
+-- |
+-- A struct with time and location components.
+data LocalTimeStruct =
+     LocalTimeStruct
+       { _lt_hour :: {-# UNPACK #-} !Hour
+       , _lt_min  :: {-# UNPACK #-} !Minute
+       , _lt_sec  :: {-# UNPACK #-} !Double
+       , _lt_zone ::                !TimeZone
+       } deriving (Eq, Generic, Ord, Show)
+
+-- |
+-- A struct with date, time and location components.
+data LocalDateTimeStruct (calendar :: Calendar) =
+     LocalDateTimeStruct
+       { _ldt_year :: {-# UNPACK #-} !Year
+       , _ldt_mon  ::                !(Month calendar)
+       , _ldt_mday :: {-# UNPACK #-} !Day
+       , _ldt_wday ::                !(DayOfWeek calendar)
+       , _ldt_hour :: {-# UNPACK #-} !Hour
+       , _ldt_min  :: {-# UNPACK #-} !Minute
+       , _ldt_sec  :: {-# UNPACK #-} !Double
+       , _ldt_zone ::                !TimeZone
+       } deriving Generic
+
+deriving instance (Eq   (Month               calendar),
+                   Eq   (DayOfWeek           calendar)) =>
+                   Eq   (DateStruct          calendar)
+
+deriving instance (Eq   (Month               calendar),
+                   Eq   (DayOfWeek           calendar)) =>
+                   Eq   (DateTimeStruct      calendar)
+
+deriving instance (Eq   (Month               calendar),
+                   Eq   (DayOfWeek           calendar)) =>
+                   Eq   (LocalDateStruct     calendar)
+
+deriving instance (Eq   (Month               calendar),
+                   Eq   (DayOfWeek           calendar)) =>
+                   Eq   (LocalDateTimeStruct calendar)
+
+deriving instance (Ord  (Month               calendar),
+                   Ord  (DayOfWeek           calendar)) =>
+                   Ord  (DateStruct          calendar)
+
+deriving instance (Ord  (Month               calendar),
+                   Ord  (DayOfWeek           calendar)) =>
+                   Ord  (DateTimeStruct      calendar)
+
+deriving instance (Ord  (Month               calendar),
+                   Ord  (DayOfWeek           calendar)) =>
+                   Ord  (LocalDateStruct     calendar)
+
+deriving instance (Ord  (Month               calendar),
+                   Ord  (DayOfWeek           calendar)) =>
+                   Ord  (LocalDateTimeStruct calendar)
+
+deriving instance (Show (Month               calendar),
+                   Show (DayOfWeek           calendar)) =>
+                   Show (DateStruct          calendar)
+
+deriving instance (Show (Month               calendar),
+                   Show (DayOfWeek           calendar)) =>
+                   Show (DateTimeStruct      calendar)
+
+deriving instance (Show (Month               calendar),
+                   Show (DayOfWeek           calendar)) =>
+                   Show (LocalDateStruct     calendar)
+
+deriving instance (Show (Month               calendar),
+                   Show (DayOfWeek           calendar)) =>
+                   Show (LocalDateTimeStruct calendar)
 
 -- |
 -- Decompose a floating point number into second and millisecond components.
