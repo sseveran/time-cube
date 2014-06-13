8d10f73b6bf4e6b95bd428c02a5224d6ff8a3131
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-12 18:48:39 -0700

    Hacking support for time zone offsets

--- Data/Time/Cube/Zone.hs
+++ Data/Time/Cube/Zone.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE NamedFieldPuns  #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# OPTIONS -Wall            #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# OPTIONS -Wall              #-}
 
 -- |
 -- Module      : Data.Time.Cube.Zone
@@ -19,14 +21,20 @@ module Data.Time.Cube.Zone (
 
  -- ** Abbreviations
      , TimeZoneAbbr(..)
-     , unabbreviate
      , abbreviate
+     , unabbreviate
 
      ) where
 
-import Data.Map.Strict ((!), fromDistinctAscList)
+import Control.Applicative ((<|>), (*>))
+import Control.Arrow ((***))
+import Control.Monad (replicateM)
+import Data.Attoparsec.Text
+import Data.Int (Int64)
+import Data.Text (Text, pack, unpack)
 import Data.Time.Cube.City (City(..))
 import GHC.Generics (Generic)
+import Text.Printf (printf)
 
 -- |
 -- Time zones.
@@ -75,6 +83,7 @@ data TimeZone =
    | MountainStandardTime
    | NewZealandDaylightTime
    | NewZealandStandardTime
+   | OffsetTime {getOffset :: {-# UNPACK #-} !Int64}
    | PacificDaylightTime
    | PacificStandardTime
    | PakistanStandardTime
@@ -83,7 +92,7 @@ data TimeZone =
    | SouthAfricaStandardTime
    | WestAfricaTime
    | YukonStandardTime
-   deriving (Eq, Enum, Generic, Ord, Read, Show)
+   deriving (Eq, Generic, Ord, Read, Show)
 
 -- |
 -- The UTC time zone.
@@ -93,18 +102,76 @@ utc = CoordinatedUniversalTime
 -- |
 -- Time zone abbreviation.
 data TimeZoneAbbr = TimeZoneAbbr {
-     tzCity :: City   -- ^ Reference location
-   , tzAbbr :: String -- ^ Time zone abbreviation string
+     tzCity :: City -- ^ Reference location
+   , tzAbbr :: Text -- ^ Time zone abbreviation string
    } deriving (Eq, Generic)
 
 instance Show TimeZoneAbbr where
-   show TimeZoneAbbr{tzAbbr} = tzAbbr
+   show TimeZoneAbbr{tzAbbr} = unpack tzAbbr
+
+-- |
+-- Abbreviate a time zone.
+abbreviate :: TimeZone -> TimeZoneAbbr
+abbreviate = \ case
+   AfghanistanTime            -> TimeZoneAbbr Kabul        "AFT"
+   AlaskaDaylightTime         -> TimeZoneAbbr Anchorage    "AKDT"
+   AlaskaHawaiiDaylightTime   -> TimeZoneAbbr Anchorage    "AHDT"
+   AlaskaHawaiiStandardTime   -> TimeZoneAbbr Anchorage    "AHST"
+   AlaskaStandardTime         -> TimeZoneAbbr Anchorage    "AKST"
+   ArabiaDaylightTime         -> TimeZoneAbbr Baghdad      "ADT"
+   ArabiaStandardTime         -> TimeZoneAbbr Riyadh       "AST"
+   BrasiliaSummerTime         -> TimeZoneAbbr SaoPaulo     "BRST"
+   BrasiliaTime               -> TimeZoneAbbr SaoPaulo     "BRT"
+   BritishSummerTime          -> TimeZoneAbbr London       "BST"
+   CentralAfricaTime          -> TimeZoneAbbr Gaborone     "CAT"
+   CentralDaylightTime        -> TimeZoneAbbr Chicago      "CDT"
+   CentralEuropeanSummerTime  -> TimeZoneAbbr Paris        "CEST"
+   CentralEuropeanTime        -> TimeZoneAbbr Paris        "CET"
+   CentralStandardTime        -> TimeZoneAbbr Chicago      "CST"
+   ChinaDaylightTime          -> TimeZoneAbbr Shanghai     "CDT"
+   ChinaStandardTime          -> TimeZoneAbbr Shanghai     "CST"
+   CoordinatedUniversalTime   -> TimeZoneAbbr Universal    "UTC"
+   EastAfricaTime             -> TimeZoneAbbr Mogadishu    "EAT"
+   EasternDaylightTime        -> TimeZoneAbbr NewYork      "EDT"
+   EasternEuropeanSummerTime  -> TimeZoneAbbr Sofia        "EEST"
+   EasternEuropeanTime        -> TimeZoneAbbr Sofia        "EET"
+   EasternStandardTime        -> TimeZoneAbbr NewYork      "EST"
+   FurtherEasternEuropeanTime -> TimeZoneAbbr Minsk        "FET"
+   GreenwichMeanTime          -> TimeZoneAbbr London       "GMT"
+   GulfStandardTime           -> TimeZoneAbbr Manama       "GST"
+   HawaiiAleutianStandardTime -> TimeZoneAbbr Honolulu     "HST"
+   HongKongSummerTime         -> TimeZoneAbbr HongKong     "HKST"
+   HongKongTime               -> TimeZoneAbbr HongKong     "HKT"
+   IndiaStandardTime          -> TimeZoneAbbr Kolkata      "IST"
+   IranDaylightTime           -> TimeZoneAbbr Tehran       "IRDT"
+   IranStandardTime           -> TimeZoneAbbr Tehran       "IRST"
+   IsraelDaylightTime         -> TimeZoneAbbr TelAviv      "IDT"
+   IsraelStandardTime         -> TimeZoneAbbr TelAviv      "IST"
+   JapanStandardTime          -> TimeZoneAbbr Tokyo        "JST"
+   KarachiTime                -> TimeZoneAbbr Karachi      "KART"
+   KoreaDaylightTime          -> TimeZoneAbbr Seoul        "KDT"
+   KoreaStandardTime          -> TimeZoneAbbr Seoul        "KST"
+   MoscowDaylightTime         -> TimeZoneAbbr Moscow       "MSD"
+   MoscowStandardTime         -> TimeZoneAbbr Moscow       "MSK"
+   MountainDaylightTime       -> TimeZoneAbbr Denver       "MDT"
+   MountainStandardTime       -> TimeZoneAbbr Denver       "MST"
+   NewZealandDaylightTime     -> TimeZoneAbbr Auckland     "NZDT"
+   NewZealandStandardTime     -> TimeZoneAbbr Auckland     "NZST"
+   OffsetTime offset          -> TimeZoneAbbr Universal $ showOffset offset
+   PacificDaylightTime        -> TimeZoneAbbr LosAngeles   "PDT"
+   PacificStandardTime        -> TimeZoneAbbr LosAngeles   "PST"
+   PakistanStandardTime       -> TimeZoneAbbr Karachi      "PKT"
+   PakistanSummerTime         -> TimeZoneAbbr Karachi      "PKST"
+   SingaporeTime              -> TimeZoneAbbr Singapore    "SGT"
+   SouthAfricaStandardTime    -> TimeZoneAbbr Johannesburg "SAST"
+   WestAfricaTime             -> TimeZoneAbbr Luanda       "WAT"
+   YukonStandardTime          -> TimeZoneAbbr Anchorage    "YST"
 
 -- |
 -- Unabbreviate a time zone. An error string is returned for unmatched abbreviations.
 unabbreviate :: TimeZoneAbbr -> Either String TimeZone
-unabbreviate (TimeZoneAbbr city abbr) = 
-   case abbr of
+unabbreviate TimeZoneAbbr{..} = 
+   case tzAbbr of
         "AFT"  -> Right AfghanistanTime
         "AHDT" -> Right AlaskaHawaiiDaylightTime
         "AHST" -> Right AlaskaHawaiiStandardTime
@@ -116,16 +183,16 @@ unabbreviate (TimeZoneAbbr city abbr) =
         "BRT"  -> Right BrasiliaTime
         "BST"  -> Right BritishSummerTime
         "CAT"  -> Right CentralAfricaTime
-        "CDT"  -> case city of
+        "CDT"  -> case tzCity of
                        Chicago  -> Right CentralDaylightTime
                        Shanghai -> Right ChinaDaylightTime
-                       _        -> f
+                       _        -> e
         "CEST" -> Right CentralEuropeanSummerTime
         "CET"  -> Right CentralEuropeanTime
-        "CST"  -> case city of
+        "CST"  -> case tzCity of
                        Chicago  -> Right CentralStandardTime
                        Shanghai -> Right ChinaStandardTime
-                       _        -> f
+                       _        -> e
         "EAT"  -> Right EastAfricaTime
         "EDT"  -> Right EasternDaylightTime
         "EEST" -> Right EasternEuropeanSummerTime
@@ -140,10 +207,10 @@ unabbreviate (TimeZoneAbbr city abbr) =
         "IDT"  -> Right IsraelDaylightTime
         "IRDT" -> Right IranDaylightTime
         "IRST" -> Right IranStandardTime
-        "IST"  -> case city of
+        "IST"  -> case tzCity of
                        Kolkata -> Right IndiaStandardTime
                        TelAviv -> Right IsraelStandardTime
-                       _       -> f
+                       _       -> e
         "JST"  -> Right JapanStandardTime
         "KART" -> Right KarachiTime
         "KDT"  -> Right KoreaDaylightTime
@@ -163,63 +230,25 @@ unabbreviate (TimeZoneAbbr city abbr) =
         "UTC"  -> Right CoordinatedUniversalTime
         "WAT"  -> Right WestAfricaTime
         "YST"  -> Right YukonStandardTime
-        _      -> Left "unabbreviate: unknown time zone abbreviation string"
-        where f = Left "unabbreviate: bad reference location"
+        _      -> flip parseOnly tzAbbr $ parseOffset <|>
+                  fail "unabbreviate: unknown time zone abbreviation string"
+        where e = Left "unabbreviate: bad reference location"
 
 -- |
--- Abbreviate a time zone.
-abbreviate :: TimeZone -> TimeZoneAbbr
-abbreviate = (!) $ fromDistinctAscList
-   [ (AfghanistanTime,TimeZoneAbbr Kabul "AFT")
-   , (AlaskaDaylightTime,TimeZoneAbbr Anchorage "AKDT")
-   , (AlaskaHawaiiDaylightTime,TimeZoneAbbr Anchorage "AHDT")
-   , (AlaskaHawaiiStandardTime,TimeZoneAbbr Anchorage "AHST")
-   , (AlaskaStandardTime,TimeZoneAbbr Anchorage "AKST")
-   , (ArabiaDaylightTime,TimeZoneAbbr Baghdad "ADT")
-   , (ArabiaStandardTime,TimeZoneAbbr Riyadh "AST")
-   , (BrasiliaSummerTime,TimeZoneAbbr SaoPaulo "BRST")
-   , (BrasiliaTime,TimeZoneAbbr SaoPaulo "BRT")
-   , (BritishSummerTime,TimeZoneAbbr London "BST")
-   , (CentralAfricaTime,TimeZoneAbbr Gaborone "CAT")
-   , (CentralDaylightTime,TimeZoneAbbr Chicago "CDT")
-   , (CentralEuropeanSummerTime,TimeZoneAbbr Paris "CEST")
-   , (CentralEuropeanTime,TimeZoneAbbr Paris "CET")
-   , (CentralStandardTime,TimeZoneAbbr Chicago "CST")
-   , (ChinaDaylightTime,TimeZoneAbbr Shanghai "CDT")
-   , (ChinaStandardTime,TimeZoneAbbr Shanghai "CST")
-   , (CoordinatedUniversalTime,TimeZoneAbbr Universal "UTC")
-   , (EastAfricaTime,TimeZoneAbbr Mogadishu "EAT")
-   , (EasternDaylightTime,TimeZoneAbbr NewYork "EDT")
-   , (EasternEuropeanSummerTime,TimeZoneAbbr Sofia "EEST")
-   , (EasternEuropeanTime,TimeZoneAbbr Sofia "EET")
-   , (EasternStandardTime,TimeZoneAbbr NewYork "EST")
-   , (FurtherEasternEuropeanTime,TimeZoneAbbr Minsk "FET")
-   , (GreenwichMeanTime,TimeZoneAbbr London "GMT")
-   , (GulfStandardTime,TimeZoneAbbr Manama "GST")
-   , (HawaiiAleutianStandardTime,TimeZoneAbbr Honolulu "HST")
-   , (HongKongSummerTime,TimeZoneAbbr HongKong "HKST")
-   , (HongKongTime,TimeZoneAbbr HongKong "HKT")
-   , (IndiaStandardTime,TimeZoneAbbr Kolkata "IST")
-   , (IranDaylightTime,TimeZoneAbbr Tehran "IRDT")
-   , (IranStandardTime,TimeZoneAbbr Tehran "IRST")
-   , (IsraelDaylightTime,TimeZoneAbbr TelAviv "IDT")
-   , (IsraelStandardTime,TimeZoneAbbr TelAviv "IST")
-   , (JapanStandardTime,TimeZoneAbbr Tokyo "JST")
-   , (KarachiTime,TimeZoneAbbr Karachi "KART")
-   , (KoreaDaylightTime,TimeZoneAbbr Seoul "KDT")
-   , (KoreaStandardTime,TimeZoneAbbr Seoul "KST")
-   , (MoscowDaylightTime,TimeZoneAbbr Moscow "MSD")
-   , (MoscowStandardTime,TimeZoneAbbr Moscow "MSK")
-   , (MountainDaylightTime,TimeZoneAbbr Denver "MDT")
-   , (MountainStandardTime,TimeZoneAbbr Denver "MST")
-   , (NewZealandDaylightTime,TimeZoneAbbr Auckland "NZDT")
-   , (NewZealandStandardTime,TimeZoneAbbr Auckland "NZST")
-   , (PacificDaylightTime,TimeZoneAbbr LosAngeles "PDT")
-   , (PacificStandardTime,TimeZoneAbbr LosAngeles "PST")
-   , (PakistanStandardTime,TimeZoneAbbr Karachi "PKT")
-   , (PakistanSummerTime,TimeZoneAbbr Karachi "PKST")
-   , (SingaporeTime,TimeZoneAbbr Singapore "SGT")
-   , (SouthAfricaStandardTime,TimeZoneAbbr Johannesburg "SAST")
-   , (WestAfricaTime,TimeZoneAbbr Luanda "WAT")
-   , (YukonStandardTime,TimeZoneAbbr Anchorage "YST")
-   ]
+-- Parse a UTC offset string.
+parseOffset :: Parser TimeZone
+parseOffset = do
+   sign    <- plus <|> minus
+   hours   <- replicateM 2 digit
+   minutes <- replicateM 2 digit
+   return $! OffsetTime . sign $ read hours * 60 + read minutes
+   where plus  = char '+' *> return id
+         minus = char '-' *> return negate
+
+-- |
+-- Show a UTC offset.
+showOffset :: Int64 -> Text
+showOffset offset =
+    pack . (:) sign . uncurry (++) . strs $ divMod offset 60
+    where sign = if offset < 0 then '-' else '+'
+          strs = printf "%02d" . abs *** printf "%02d"
