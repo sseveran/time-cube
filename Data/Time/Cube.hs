a890713239f53269b1df3303760952966167374d
Enzo Haussecker <ehaussecker@gmail.com>
2014-06-12 19:09:38 -0700

    Exporting lens module

--- Data/Time/Cube.hs
+++ Data/Time/Cube.hs
@@ -12,6 +12,7 @@ module Data.Time.Cube (
 
        module Data.Time.Cube.Base
      , module Data.Time.Cube.City
+     , module Data.Time.Cube.Lens
      , module Data.Time.Cube.Parser
      , module Data.Time.Cube.Unix
      , module Data.Time.Cube.Unix.Gregorian
@@ -21,6 +22,7 @@ module Data.Time.Cube (
 
 import Data.Time.Cube.Base
 import Data.Time.Cube.City
+import Data.Time.Cube.Lens
 import Data.Time.Cube.Parser
 import Data.Time.Cube.Unix
 import Data.Time.Cube.Unix.Gregorian
