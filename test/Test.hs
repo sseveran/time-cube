ba58de16898392567b03d8a677e3a4a0d79ac20d
Enzo Haussecker <ehaussecker@gmail.com>
2015-01-23 13:45:12 -0800

    setup unit testing

---
+++ test/Test.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS -Wall #-}
+
+-- |
+-- Module      : Data.Time.Cube.Base
+-- License     : BSD3
+-- Maintainer  : ehaussecker@alphaheavy.com
+-- Stability   : Experimental
+-- Portability : GHC 7.8.* on Unix
+--
+-- Time cube test suite
+
+module Main where
+
+import Data.Time.Cube
+import Foreign.C.Time
+import GHC.TypeLits.SigNat
+import Test.QuickCheck
+
+main :: IO ()
+main = return ()
