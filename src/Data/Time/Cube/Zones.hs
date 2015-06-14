be1a3f8f1146456700621eaaadad5515f8391639
enzo@Enzos-MacBook-Pro.local <enzo@Enzos-MacBook-Pro.local>
2015-06-14 11:48:08 -0700

    Revise file metadata

--- src/Data/Time/Cube/Zones.hs
+++ src/Data/Time/Cube/Zones.hs
@@ -11,12 +11,14 @@
 
 -- |
 -- Module      : Data.Time.Cube.Zones
+-- Description : Dependently typed time zones.
+-- Copyright   : Copyright (c) 2015, Alpha Heavy Industries, Inc. All rights reserved.
 -- License     : BSD3
--- Maintainer  : ehaussecker@alphaheavy.com
+-- Maintainer  : Enzo Haussecker <ehaussecker@alphaheavy.com>
 -- Stability   : Experimental
--- Portability : GHC 7.8.* on Unix
+-- Portability : Portable
 --
--- Dependently typed time zones and related utilities.
+-- This module provides time zone data types and related parameterizations.
 module Data.Time.Cube.Zones (
 
  -- ** Time Zones
