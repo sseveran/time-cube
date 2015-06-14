1647a777cbb6dc93745f2104486a67b29f97068c
enzo@Enzos-MacBook-Pro.local <enzo@Enzos-MacBook-Pro.local>
2015-06-14 12:13:56 -0700

    Revise file metadata

--- src/Data/Time/Cube/Format.hs
+++ src/Data/Time/Cube/Format.hs
@@ -2,10 +2,11 @@
 
 -- |
 -- Module      : Data.Time.Cube.Format
+-- Copyright   : Copyright (c) 2015, Alpha Heavy Industries, Inc. All rights reserved.
 -- License     : BSD3
--- Maintainer  : ehaussecker@alphaheavy.com
+-- Maintainer  : Enzo Haussecker <ehaussecker@alphaheavy.com>
 -- Stability   : Experimental
--- Portability : GHC 7.8.* on Unix
+-- Portability : Portable
 --
 -- Date and time format strings.
 module Data.Time.Cube.Format (
--- src/Data/Time/Cube/Parser.hs
+++ src/Data/Time/Cube/Parser.hs
@@ -11,10 +11,11 @@
 
 -- |
 -- Module      : Data.Time.Cube.Parser
+-- Copyright   : Copyright (c) 2015, Alpha Heavy Industries, Inc. All rights reserved.
 -- License     : BSD3
--- Maintainer  : ehaussecker@alphaheavy.com
+-- Maintainer  : Enzo Haussecker <ehaussecker@alphaheavy.com>
 -- Stability   : Experimental
--- Portability : GHC 7.8.* on Unix
+-- Portability : Portable
 --
 -- Timestamp parsing.
 module Data.Time.Cube.Parser (
