46346e7ceaca49e47ca8108c1f6115e3d69ed6e4
enzo@Enzos-MacBook-Pro.local <enzo@Enzos-MacBook-Pro.local>
2015-06-14 13:30:00 -0700

    Revise file metadata

--- src/Data/Time/Cube/Local.hs
+++ src/Data/Time/Cube/Local.hs
@@ -8,10 +8,11 @@
 
 -- |
 -- Module      : Data.Time.Cube.Local
+-- Copyright   : Copyright (c) 2015, Alpha Heavy Industries, Inc. All rights reserved.
 -- License     : BSD3
--- Maintainer  : ehaussecker@alphaheavy.com
+-- Maintainer  : Enzo Haussecker <ehaussecker@alphaheavy.com>
 -- Stability   : Experimental
--- Portability : GHC 7.8.* on Unix
+-- Portability : Portable
 --
 -- Local timestamps.
 module Data.Time.Cube.Local (
